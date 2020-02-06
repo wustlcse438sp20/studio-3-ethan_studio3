93e67da57c201ec0f78b1c3d1036be10621ede85
Ethan Kantor <ethankantor@wustl.edu>
2020-02-06 15:50:38 -0600

    finish

--- app/src/main/java/com/example/cse438/studio3/BreweryViewModel.kt
+++ app/src/main/java/com/example/cse438/studio3/BreweryViewModel.kt
@@ -5,12 +5,19 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.example.cse438_rest_studio.Network.BreweryRepository
 import com.example.cse438_rest_studio.data.Brewery
+import com.example.cse438_rest_studio.data.BreweryPayload
 
 
 class BreweryViewModel(application: Application): AndroidViewModel(application) {
 
     //TODO #8: Create a livedata object for tracking breweries
-
+    var breweryList: MutableLiveData<List<Brewery>> = MutableLiveData()
+    var breweryRepository: BreweryRepository = BreweryRepository()
     //TODO #9: Write functions to access the two brewery API calls from the repository
-
+    fun getBreweriesByCity(city: String) {
+        breweryRepository.getBreweriesByCity(breweryList, city)
+    }
+    fun breweriesSearch(term: String) {
+        breweryRepository.breweriesSearch(breweryList, term)
+    }
 }
--- app/src/main/java/com/example/cse438/studio3/Network/BreweryRepository.kt
+++ app/src/main/java/com/example/cse438/studio3/Network/BreweryRepository.kt
@@ -2,15 +2,47 @@ package com.example.cse438_rest_studio.Network
 
 import androidx.lifecycle.MutableLiveData
 import com.example.cse438_rest_studio.data.Brewery
+import com.example.cse438_rest_studio.data.BreweryPayload
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
+import android.util.Log
 
 class BreweryRepository {
-
+    val service = ApiClient.makeRetrofitService()
     //TODO #6: Create a function that launches a coroutine for searching by city
+    fun getBreweriesByCity(resBody: MutableLiveData<List<Brewery>>, city: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = service.getBreweriesByCity(city)
 
+            withContext(Dispatchers.Main) {
+                try{
+                    if(response.isSuccessful) {
+                        resBody.value = response.body()
+                        Log.d("GOT RESPONSE", response.body().toString())
+                    }
+                } catch (e: HttpException) {
+                    println("Http error")
+                }
+            }
+        }
+    }
     //TODO #7: Create a function that launches a coroutine for searching by a generic term
+    fun breweriesSearch(resBody: MutableLiveData<List<Brewery>>, term: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = service.breweriesSearch(term)
+
+            withContext(Dispatchers.Main) {
+                try{
+                    if(response.isSuccessful) {
+                        resBody.value = response.body()
+                    }
+                } catch (e: HttpException) {
+                    println("Http error")
+                }
+            }
+        }
+    }
 }
